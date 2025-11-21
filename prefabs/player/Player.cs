52dd5e2e5f1921e0885bd77a1bca0f5020e35d7b
Mohawk <Mohawk@MICHAEL>
2025-11-21 16:46:52 -0500

    Add script to move and drop player

--- prefabs/player/Player.cs
+++ prefabs/player/Player.cs
@@ -6,10 +6,29 @@ public partial class Player : RigidBody2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		Freeze = true;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		// if the disk is frozen, it's at the top and we can move it around
+		if (Freeze)
+		{
+			// copy the current position into a new position
+			Vector2 newPosition = Position;
+
+			// set the X position to be the mouse position
+			newPosition.X = GetViewport().GetMousePosition().X;
+
+			// finally, set the position as the new position
+			Position = newPosition;
+
+			// drop_disk action is bound to the Left Click
+			if (Input.IsActionJustPressed("drop_disk"))
+			{
+				Freeze = false;
+			}
+		}
 	}
 }
